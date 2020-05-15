2c8270b51327836afe94437b86d927de7b447708
elena <volchenkovae@gmail.com>
2020-05-14 22:31:54 -0500

    test

---
+++ StringPractice.java
@@ -0,0 +1,23 @@
+package SB;
+
+public class StringPractice {
+    public static void main(String[] args) {
+        String str = "tech";
+        String str2 = str;
+
+        System.out.println(str);
+        System.out.println(str2);
+
+        str = "torial";
+        System.out.println(str);
+        System.out.println(str2);
+
+        String str3 = "Welcome";
+        str3 = str3 + " back";
+        str3 = str3 + " home!";
+
+        System.out.println(str3);
+
+    }
+
+}
